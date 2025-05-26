47d27f7dae92e88a506aea40bacc80a47f4952e3
YnBn <twb72743075@126.com>
2025-05-26 16:09:19 +0800

    None

--- setup.py
+++ setup.py
@@ -1,38 +1,131 @@
-from setuptools import setup, find_packages
-from pkg_resources import parse_requirements
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open("requirements.txt", encoding="utf-8") as fp:
-	install_requires = [str(requirement) for requirement in parse_requirements(fp)]
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
 
-long_description = """
-Until 2025-4-20, just acheived these functions
-Model:bert_mask, print_tfreocrd, token, TFInputDataset
-Data:read fasta, ELECTRA fragment, slice train and eval
-"""
+import io
+import os
+import sys
+from shutil import rmtree
 
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'proteinynbn'
+DESCRIPTION = 'My short description for my project.'
+URL = 'https://github.com/me/myproject'
+EMAIL = 'me@example.com'
+AUTHOR = 'Awesome Soul'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.1.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-	name="Protein_L_YnBn",
-	version="0.0.1",
-	author="YnBn",
-	author_email="twb72743075@126.com",
-	description="some functions used frequently in protein deep learn model",
-	long_description=long_description,
-	license="MIT License",
-	url="https://github.com/KIOYnBn/Proein_L_YnBn",
-	
-	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Intended Audience :: me",
-		"Topic :: Software Development :: Deep Learn Model Tools",
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: All",
-	],
-	
-	packages=find_packages(exclude='tests.'),
-	install_requires=install_requires,
-	entry_points={
-		'console_scripts': [
-			'test = test.help:main'
-		]
-	})
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
